2542d268390b7ed397e8bcf9538ce0e6d3bb0e0b
beau currier <beaucurrier01@gmail.com>
2024-09-26 17:15:44 -0700

    setting up the todo list

--- ToDoListBackend/app.js
+++ ToDoListBackend/app.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 app.use(express.json)
 mongoose.connect(`${process.env.MONGODB_API_KEY}`)
 app.use(cors())
-app.get("todos", async (req, res) =>{
+app.get("/todos", async (req, res) =>{
     const todos = await Todos.find();
     res.json({todos:todos});
 })
@@ -17,7 +17,7 @@ app.post("./add-item", async (req, res) => {
 app.post("./edit-item/:id", async (req, res) => {
     await Todos.findOneAndUpdate({id:req.params.id}, req.body)
 })
-app.post("./delete-item", async (req, res) => {
+app.delete("./delete-item/:id", async (req, res) => {
     await Todos.deleteOne({id:req.params.id})
 })
 app.listen(5000,() =>{
