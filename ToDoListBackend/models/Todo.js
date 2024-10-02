148ce57da1b66fd1600f7030a857a961697a59a5
beau currier <beaucurrier01@gmail.com>
2024-10-01 17:25:03 -0700

    working on fixing errors

--- ToDoListBackend/models/Todo.js
+++ ToDoListBackend/models/Todo.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
-const todoSchema = new Schema({id:Number, text:String, nmae:String, completed:Boolean})
+const todoSchema = new Schema({id:Number, text:{type:String, require: true}, name:String, completed:{type:Boolean, default:false}},{timestamp:true})
 module.exports = mongoose.model("Todo", todoSchema)
