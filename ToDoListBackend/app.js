3eab108508d79c8ad859468343263a2094d29534
beau currier <beaucurrier01@gmail.com>
2024-10-03 17:53:46 -0700

    cors

--- ToDoListBackend/app.js
+++ ToDoListBackend/app.js
@@ -6,16 +6,17 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 app.use(express.json)
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_API_KEY}@cluster0.fw3p5.mongodb.net/`)
-app.use(cors())
+const corsOptions = {origin: 'https://react-projects-ten-lemon.vercel.app/', optionsSuccessStatus: 200}
+app.use(cors(corsOptions))
 app.get("/todo", async (req, res) =>{
     const todos = await Todos.find();
     res.json({todos:todos});
 })
-app.post("./add-item", async (req, res) => {
+app.post("/add-item", async (req, res) => {
     const newToDo = await Todos.create(req.body)
     res.json(newToDo)
 })
-app.post("./edit-item/:id", async (req, res) => {
+app.post("/edit-item/:id", async (req, res) => {
     const updatedTodo = await Todos.findOneAndUpdate(
         { _id: req.params.id },
         { $set: { text: req.body.text, completed: req.body.completed } }, // Update both task and completed status
@@ -23,7 +24,7 @@ app.post("./edit-item/:id", async (req, res) => {
       );
       res.json(updatedTodo);
 })
-app.delete("./delete-item/:id", async (req, res) => {
+app.delete("/delete-item/:id", async (req, res) => {
     await Todos.deleteOne({_id:req.params.id})
 })
 app.listen(5000,() =>{
