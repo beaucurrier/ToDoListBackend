3bbea09499c450f5c530893364dee9525cbff4db
beau currier <beaucurrier01@gmail.com>
2024-10-10 16:28:43 -0700

    changed code to match steves code

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const Todos = require("./models/Todo")
-const mongoose = require("mongoose")
-require("dotenv").config()
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_API_KEY}@cluster0.fw3p5.mongodb.net/`)
-const corsOptions = {origin: '*', optionsSuccessStatus: 200}
-app.use(cors(corsOptions))
-app.use(express.json())
+const Todos = require("./models/Todos");
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.connect(`${process.env.MONGODB_API_KEY}`);
+const PORT = process.env.PORT || 5000;
+const corsOptions = {
+  origin: "https://steventodo.netlify.app",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 
-
-app.get("/todos", async (req, res) =>{
-    const todos = await Todos.find();
-    res.json({todos:todos});
-})
+app.get("/todos", async (req, res) => {
+  const todos = await Todos.find();
+  res.json({ todos: todos });
+});
 app.post("/add-item", async (req, res) => {
-    const newToDo = await Todos.create(req.body)
-    res.json(newToDo)
-})
+  const newTodo = await Todos.create(req.body);
+  res.json(newTodo);
+});
 app.post("/edit-item/:id", async (req, res) => {
+  try {
     const updatedTodo = await Todos.findOneAndUpdate(
-        { _id: req.params.id },
-        { $set: { text: req.body.text, completed: req.body.completed } }, // Update both task and completed status
-        { new: true }
-      );
-      res.json(updatedTodo);
-})
+      { _id: req.params.id },
+      { $set: { text: req.body.text, completed: req.body.completed } },
+      { new: true }
+    );
+    res.json(updatedTodo);
+  } catch (error) {
+    res.status(500).json({ error: "failed to update todo" });
+  }
+});
 app.delete("/delete-item/:id", async (req, res) => {
-    await Todos.deleteOne({_id:req.params.id})
-})
-app.listen(5000,() =>{
-    console.log('server is running on Port 5000')
-})
-
+  await Todos.deleteOne({ _id: req.params.id });
+  res.json({ message: "todo was deleted successfully!" });
+});
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("server is running on Port 5000");
+});
--- models/Todo.js
+++ models/Todo.js
@@ -1,4 +1,10 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
-const todoSchema = new Schema({ text:{type:String, required: true}, completed:{type:Boolean, default:false}},{timestamp:true})
-module.exports = mongoose.model("Todo", todoSchema)
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const todoSchema = new Schema(
+  {
+    text: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Todos", todoSchema);
