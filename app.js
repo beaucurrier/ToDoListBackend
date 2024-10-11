c27f3ab52aa36e1a012113e9d8d06c9683ec9a54
beau currier <beaucurrier01@gmail.com>
2024-10-10 17:47:31 -0700

    credentials

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: "react-projects-ten-lemon.vercel.app",
   optionsSuccessStatus: 200,
-  credtials: true
+  credentials: true
 };
 app.use(cors(corsOptions));
 app.use(express.json());
